6babf8d34bb2abc7c4100607748cb24504cba222
许弘毅 <383164014@qq.com>
2021-03-25 16:15:51 +0800

    新增调试调用栈

--- recovery/recovery.go
+++ recovery/recovery.go
@@ -4,12 +4,18 @@ import (
 	grpc_recovery "github.com/grpc-ecosystem/go-grpc-middleware/recovery"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"runtime"
 )
 
 // RecoveryInterceptor panic时返回Unknown错误吗
 func RecoveryInterceptor() grpc_recovery.Option {
 	return grpc_recovery.WithRecoveryHandler(func(p interface{}) (err error) {
+		buf := make([]byte, 4096)
+		// 抛出服务端 调用栈的轨迹
+		// runtime.stack 详情参考资料
+		// https://colobu.com/2016/12/21/how-to-dump-goroutine-stack-traces/
+		num := runtime.Stack(buf, false)
 		// p 就是捕获 panic 中的内容
-		return status.Errorf(codes.Unknown, "panic 了: %v", p)
+		return status.Errorf(codes.Unknown, "panic 了: %v %s", p,string(buf[:num]))
 	})
 }
